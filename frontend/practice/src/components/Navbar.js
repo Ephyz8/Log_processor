5a8a1c63930c51ee5845649585a24edb6d9d0ead
Ephyz8 <ziwoyaephraim@gmail.com>
2024-11-17 05:39:26 +0200

    Navbar

--- frontend/practice/src/components/Navbar.js
+++ frontend/practice/src/components/Navbar.js
@@ -3,7 +3,9 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/Icon-nahese.png" />
+            <img src="../images/NANOHEALTH.png" alt="Company logo" />
+            <h3 className="nav--icon">Nano Health</h3>
+            <h4 className="nav--links">About Contact Login Register</h4>
         </nav>
     )
-}  
+}
