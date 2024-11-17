681cccfb44c1016058ddeb24ab377c882a8c4604
Ephyz8 <ziwoyaephraim@gmail.com>
2024-11-17 04:41:59 +0200

    practice

---
+++ frontend/practice/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Main() {
+    return(
+        <div>
+            <h1>
+                EPHRAIM ON IT
+            </h1>
+        </div>
+    )
+}
